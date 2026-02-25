cddd630d50f2eae61300f82af0a81e9fdc52508a
cookooyas <cookooyas@gmail.com>
2026-02-25 15:21:21 +0900

    initial commit

---
+++ src/test/java/com/example/vtlt/VtltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vtlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
